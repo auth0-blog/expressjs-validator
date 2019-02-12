435bd498973ace4b5ebceaec5dfbc00da08b97fc
Vladimir Fomene <vladimirfomene@gmail.com>
2019-02-12 09:56:15 +0100

    add routes, add app setup in app.js, add pugjs and javascript.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Mount routers
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+app.listen(port, () => console.log(`Expressjs-Validator listening on port ${port}!`));
+
+module.exports = app;
